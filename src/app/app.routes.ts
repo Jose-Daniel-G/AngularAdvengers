46f6ef9cfdadbfe5c3e771a295955d477b5d134e
Jose-Daniel-G <jose.grijalba04@gmail.com>
2024-11-13 16:12:44 -0500

    Carrito de Compras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CatalogoInicioComponent } from './components/catalogo/catalogo-inicio/catalogo-inicio.component';
+import { CarritoListadoComponent } from './components/carrito/carrito-listado/carrito-listado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:CatalogoInicioComponent},
+    { path: 'carrito', component:CarritoListadoComponent},
+    { path: '**', pathMatch: 'full',redirectTo: '' },
+];
